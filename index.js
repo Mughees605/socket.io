f4f0d83106f5284690e4e03a24828121811f8cee
mughees605 <mughees605@gmail.com>
2018-05-11 23:21:40 +0500

    added express

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser());
-app.use(session({secret:"fd34s@!@dfa453f3DF#$D&W",resave:false,saveUninitialized:true}))
 
 app.listen(port,() => {
     console.log(port)
